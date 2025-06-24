38d78f84054913b00c64030b5df3489b353c4cc4
Hendricks_01 <91264904+Hendricks-01@users.noreply.github.com>
2025-06-24 19:17:32 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,24 @@
+#1.Declare a variable named age and assign your age to it.
+age = 24
+print(age) 
+
+
+#2.Create two variables x = 10 and y = 5. Print their sum.
+x= 10
+y = 5
+print(x + y)  # Print the sum of x and y
+
+
+#3.Try using an invalid variable name like 2ndName and observe the error.
+# This will raise a SyntaxError because variable names cannot start with a digit
+# 2ndName = "hendricks"  # Uncommenting this line will raise an error
+
+#4. Assign a string to a variable and print it.
+my_string = "Hello, world!"
+print(my_string)
+
+#Challenge Write a program that asks the user to input their name and age, and then prints a greeting.
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hello, {name}! You are {age} years old.")
